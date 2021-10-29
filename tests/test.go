7a7c0b88aaddd92404df4947052230fa85d7cc63
loutr <l.ta-ma@protonmail.com>
2021-10-29 14:51:43 +0200

    Initial commit

---
+++ tests/test.go
@@ -0,0 +1,13 @@
+func fact(n int) int {
+    if n <= 1 {
+	return 1;
+    }
+    return n * fact (n - 1);
+}
+
+func main () {
+    for n := 0; n <= 10; n++ {
+	fmt.Print(fact(n));
+	fmt.Print("\n");
+    }
+}
